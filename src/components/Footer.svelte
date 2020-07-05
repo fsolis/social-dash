88ccef10af03b80559c8c68a044cb3aa5babc711
Freddy <fsolis.se@gmail.com>
2020-07-05 15:48:11 -0700

    Updated footer links

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,9 +1,11 @@
 <div class="attribution">
   Challenge by
-  <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+  <a
+    href="https://www.frontendmentor.io/challenges/social-media-dashboard-with-theme-switcher-6oY8ozp_H"
+    target="_blank">
     Frontend Mentor
   </a>
   . Coded by
-  <a href="#">Freddy</a>
+  <a href="https://github.com/fsolis/social-dash">Freddy</a>
   .
 </div>
